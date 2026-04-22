cd9e6226699c8c04748d7c198d2937ca45329d7a
ameena <ameenababuraj@gmail.com>
2026-04-22 12:42:44 +0530

    Create adding 2 nos.c

---
+++ adding 2 nos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int a, b, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d", sum);
+    return 0;
+}
+/*
+output:
+Enter two integers: 5
+6
+Sum = 11
+  */
